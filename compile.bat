d779adbbe860288bbb763f7a7b7d63dd84f8ea8f
wangji <544715907@qq.com>
2017-11-22 21:12:04 +0800

    fix

--- compile.bat
+++ compile.bat
@@ -1,8 +1,9 @@
-javac -extdirs lib/ core/*.java
-javac -extdirs lib/ movement/*.java
-javac -extdirs lib/ report/*.java
-javac -extdirs lib/ routing/*.java
-javac -extdirs lib/ gui/*.java
-javac -extdirs lib/ input/*.java
-javac -extdirs lib/ applications/*.java
-javac -extdirs lib/ interfaces/*.java
+
+javac  -extdirs lib/ core/*.java
+javac  -extdirs lib/ movement/*.java
+javac  -extdirs lib/ report/*.java
+javac  -extdirs lib/ routing/*.java
+javac  -extdirs lib/ gui/*.java
+javac  -extdirs lib/ input/*.java
+javac  -extdirs lib/ applications/*.java
+javac  -extdirs lib/ interfaces/*.java
