cf3b368673603d1e4c4727f3fb90107fa76efe35
wangji <544715907@qq.com>
2017-11-22 21:49:50 +0800

    add compile.sh

--- compile.bat
+++ compile.bat
@@ -1,9 +1,12 @@
+set targetdir=target
 
-javac  -extdirs lib/ core/*.java
-javac  -extdirs lib/ movement/*.java
-javac  -extdirs lib/ report/*.java
-javac  -extdirs lib/ routing/*.java
-javac  -extdirs lib/ gui/*.java
-javac  -extdirs lib/ input/*.java
-javac  -extdirs lib/ applications/*.java
-javac  -extdirs lib/ interfaces/*.java
+IF NOT EXIST "%targetdir%" mkdir %targetdir%
+
+javac -sourcepath src -d %targetdir% -extdirs lib/ src/core/*.java src/movement/*.java src/report/*.java src/routing/*.java src/gui/*.java src/input/*.java src/applications/*.java src/interfaces/*.java
+
+
+
+IF NOT EXIST "%targetdir%\gui\buttonGraphics" (
+	mkdir %targetdir%\gui\buttonGraphics
+	copy src\gui\buttonGraphics\* %targetdir%\gui\buttonGraphics\
+)
